f03024b170b50ecf1eab618a4f371e52a674380d
Iemontine <computereality@gmail.com>
2025-05-24 20:22:32 -0700

    Add MLP model definition and loading functionality
    - Introduced a new MLP class for a simple feedforward neural network.
    - Added a method to load pre-trained weights from 'data/moon_classifier_mlp.pth'.
    - Updated imports to include PyTorch and its neural network module.

--- main.py
+++ main.py
@@ -4,41 +4,34 @@ import torch
 from torch import nn
 import torchvision
 
-from heuristics import ActivationBased, SetHeuristic
+from heuristics import FromLayerHeuristic, SetHeuristic
 
 ROOT = "./data"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def main():
-    loader = Loader(
-        dataset=torchvision.datasets.MNIST(
-            root=ROOT,
-            train=True,
-            download=True,
-            transform=torchvision.transforms.Compose([
-                torchvision.transforms.ToTensor(),
-                torchvision.transforms.Lambda(lambda t: t.flatten(-3,-1)),
-            ])
-        ),
-        model = nn.Sequential(
-            nn.Linear(784, 100),
-            nn.ReLU(),
-            nn.Linear(100, 100),
-            nn.ReLU(),
-            nn.Linear(100, 100),
-            nn.ReLU(),
-            nn.Linear(100, 100),
-            nn.ReLU(),
-            nn.Linear(100, 100),
-            nn.ReLU(),
-            nn.Linear(100, 10),
-        ).to(device=DEVICE, dtype=torch.float32),
-        edit_heuristic=ActivationBased,
-        set_heuristic=SetHeuristic, # TODO: implement a different set heuristic
+    mlp_test()
+
+def mlp_test():
+    model = models.mlp()
+
+    edit_heuristic = FromLayerHeuristic(
+        start_layer=-1,
+        lb=-0.1,
+        ub=0.1,
     )
 
-    # network = models.vit_b_32(pretrained=True, eval=True)
-    # print(network)
+    set_heuristic = SetHeuristic(
+        filename=f"{ROOT}/moon_misclassifications.pt",
+        device=DEVICE,
+        dtype=torch.float32,
+    )
+
+    loader = Loader(
+        model=models.mlp(),
+        edit_heuristic=edit_heuristic,
+        set_heuristic=set_heuristic,
+    )
 
 
 if __name__ == "__main__":
