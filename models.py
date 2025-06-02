e7b3cc3a28a29c15933c41fab822e8820b0af745
Iemontine <computereality@gmail.com>
2025-06-01 21:39:44 -0700

    add fulldatasetheuristic, add models from data

--- models.py
+++ models.py
@@ -6,21 +6,21 @@ import sytorch as st
 import torch
 import torch.nn as nn
 import torchvision
-from torchvision.models import ViT_B_16_Weights, ViT_B_32_Weights
+# from torchvision.models import ViT_B_16_Weights, ViT_B_32_Weights
 
-# Largest patch size (should be easier to repair and evaluate)
-def vit_b_32(pretrained: bool = True, eval: bool = True):
-    weights = ViT_B_32_Weights.DEFAULT if pretrained else None
-    network = torchvision.models.vit_b_32(weights=weights).train(mode = not eval)
-    # network = st.nn.to_editable(network)
-    return network
+# # Largest patch size (should be easier to repair and evaluate)
+# def vit_b_32(pretrained: bool = True, eval: bool = True):
+#     weights = ViT_B_32_Weights.DEFAULT if pretrained else None
+#     network = torchvision.models.vit_b_32(weights=weights).train(mode = not eval)
+#     # network = st.nn.to_editable(network)
+#     return network
 
-# Use if vit_b_32 is too coarse
-def vit_b_16(pretrained: bool = True, eval: bool = True):
-    weights = ViT_B_16_Weights.DEFAULT if pretrained else None
-    network = torchvision.models.vit_b_16(weights=weights).train(mode = not eval)
-    # network = st.nn.to_editable(network)
-    return network
+# # Use if vit_b_32 is too coarse
+# def vit_b_16(pretrained: bool = True, eval: bool = True):
+#     weights = ViT_B_16_Weights.DEFAULT if pretrained else None
+#     network = torchvision.models.vit_b_16(weights=weights).train(mode = not eval)
+#     # network = st.nn.to_editable(network)
+#     return network
 
 def mlp(path: Path = None,
     device: torch.device = torch.device('cpu'),
